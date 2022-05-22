d9b2936a4147d24516eee4ffafa57c05997b564f
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-05-22 16:28:20 -0400

    Create action dispatcher for topic form

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -4,24 +4,23 @@ export const topicsSlice = createSlice({
     name: 'topics',
     initialState: {
         topics: {
-            id: 1234,
-            name: "test",
-            icon: "https://google.com",
-            quizIds: []
+            
         }
     },
     reducers: {
-        addTopic: (state, action) => state.topics[action.payload.id] = {
-            id: action.payload.id,
-            name: action.payload.name,
-            icon: action.payload.icon,
-            quizIds: []
-        }
+        addTopic(state, action) {
+            state.topics[action.payload.id] = {
+                id: action.payload.id,
+                name: action.payload.name,
+                icon: action.payload.icon,
+                quizIds: []
+            }
+        } 
     }
 })
 
 const topicSliceReducer = topicsSlice.reducer;
 
-export const selectTopics = state => state.topics;
+export const selectTopics = state => state.topics.topics;
 export const { addTopic } = topicsSlice.actions;
 export default topicSliceReducer;
