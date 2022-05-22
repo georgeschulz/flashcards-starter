afe12f04195b8a523dd947a17c6de3b2a9e17ae7
georgeschulz <georgeschulz33@gmail.com>
2022-05-22 15:22:45 -0400

    Start topics slice

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic: (state, action) => state.topics[action.payload.id] = {
+            id: action.payload.id,
+            name: action.payload.name,
+            icon: action.payload.icon,
+            quizIds: []
+        }
+    }
+})
+
+export const selectTopics = state => state.topics;
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
